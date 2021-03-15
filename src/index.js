7c45176738f30c599f2b54f6194f03b216861988
orazin <zinullin.o@gmail.com>
2021-03-15 20:57:56 +0600

    fead: add 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }else{
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }else{
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }else{
+    let sumElem = array.reduce((a,b)=>a+b);
+    let findAvg = sumElem / array.length;
+    return findAvg;
+  }
 }
